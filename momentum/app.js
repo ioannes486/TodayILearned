2d6610ca4fbf8d17849556220d326af03533dd9d
ioannes486 <rhkdghks21@naver.com>
2023-09-15 18:52:08 +0900

    completed greetings

--- momentum/app.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector(".login-form input");
-const loginBtn = document.querySelector(".login-form button");
-
-function clickLoginBtn() {
-  // console.log("clicked");
-  console.log(loginInput.value);
-}
-
-loginBtn.addEventListener("click", clickLoginBtn);
